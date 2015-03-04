51bf9339595b541d36b43180d6130d0287602759
Victor Hugo <victorhugolgr@gmail.com>
2015-03-04 17:23:44 -0300

    layout

---
+++ WBAWSWeb/Smarty-3.1.21/demo/templates_c/0b78306b8ebd83ceda88ef61c30e679fed495089.file.header.tpl.cache.php
@@ -0,0 +1,35 @@
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-03-04 20:06:16
+         compiled from ".\templates\header.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:1585754f765b8eb1a27-50912240%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '0b78306b8ebd83ceda88ef61c30e679fed495089' => 
+    array (
+      0 => '.\\templates\\header.tpl',
+      1 => 1425499499,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '1585754f765b8eb1a27-50912240',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'title' => 0,
+    'Name' => 1,
+  ),
+  'has_nocache_code' => true,
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_54f765b8ec1466_06415360',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_54f765b8ec1466_06415360')) {function content_54f765b8ec1466_06415360($_smarty_tpl) {?><HTML>
+<HEAD>
+    <TITLE><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+ - <?php echo '/*%%SmartyNocache:1585754f765b8eb1a27-50912240%%*/<?php echo $_smarty_tpl->tpl_vars[\'Name\']->value;?>
+/*/%%SmartyNocache:1585754f765b8eb1a27-50912240%%*/';?>
+</TITLE>
+</HEAD>
+<BODY bgcolor="#ffffff">
+<?php }} ?>
--- WBAWSWeb/index.php
+++ WBAWSWeb/index.php
@@ -1,18 +1,12 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-            echo 'Bem Vindo!!!';
-            echo "projeto AWS";
-        ?>
-    </body>
-</html>
+<?php
+
+include './corp/Smarty/libs/Smarty.class.php';
+
+$smarty = new Smarty;
+
+//$smarty->force_compile = true;
+$smarty->debugging = true;
+$smarty->caching = true;
+$smarty->cache_lifetime = 120;
+
+$smarty->display('./AWSWeb/index.html');
