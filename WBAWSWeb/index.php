e99e4c5a81d795c4f7a05240f749226171d5cb2a
Waldemir Botelho <romeuwb@msn.com>
2015-03-04 19:52:42 -0300

    Correções enderços

--- WBAWSWeb/index.php
+++ WBAWSWeb/index.php
@@ -2,7 +2,7 @@
 
 include './corp/Smarty/libs/Smarty.class.php';
 
-$smarty = new Smarty;
+$smarty = new Smarty();
 
 $smarty->template_dir = "smarty/templates";
 $smarty->compile_dir = "smarty/templates_c";
