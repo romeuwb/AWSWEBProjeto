3aaf8801a477efc4d393c5791a10a7fc48bd3988
Victor Hugo <victorhugolgr@gmail.com>
2015-03-05 01:01:59 -0300

    projeto inicial.

---
+++ WBAWSWeb/db/dbConnection.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ *Autor: Victor Hugo
+ * Data: 04/03/2015
+ * Descrição: Classe responsável pela conexão ao banco de dados. 
+ */
+require 'config/config.php';
+class dbConnection extends config{
+    
+    private $user = "root";
+    private $pass = "root";
+    private $host = "127.0.0.1";
+    private $database = "WBAWSWeb";
+    
+    //Conectar (privado)
+    private function Connect(){
+        $conn = new PDO("mysql:host=$this->host;dbname=$this->database",  $this->user, $this->pass);
+        return $conn;
+    }
+    
+    //Executa uma query. Retorna a quantidade de linha executada(True).
+    public function RunQuery($sql){
+        $stm = $this->Connect()->prepare($sql);
+        return $stm->execute();
+    }
+    
+    //Executa uma select
+    public function RunSelect($sql){
+        $stm = $this->Connect()->prepare($sql);
+        $stm->execute();
+        return $stm->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
