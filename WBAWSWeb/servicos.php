8ab0995638badcb40650bff0c184366c3086dbd3
Waldemir Botelho <romeuwb@msn.com>
2015-03-05 11:00:54 -0300

    Correções enderços

--- WBAWSWeb/servicos.php
+++ WBAWSWeb/servicos.php
@@ -8,7 +8,7 @@ $smarty->template_dir = "smarty/templates";
 $smarty->compile_dir = "smarty/templates_c";
 $smarty->config_dir = "smarty/configs";
 //$smarty->force_compile = true;
-$smarty->debugging = true;
+$smarty->debugging = false;
 $smarty->caching = true;
 $smarty->cache_lifetime = 120;
 
